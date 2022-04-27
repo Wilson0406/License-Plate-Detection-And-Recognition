0dab8c42194f4a070876f69ad7a8fc58ae78f8a6
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-04-27 20:54:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,25 +30,29 @@ else:
 '''
 
     
-
+#Converting the input image to greyscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("greyed image", gray_image)
 cv2.waitKey(0)
 
+#Reducing the noise in the greyscale image
 gray_image = cv2.bilateralFilter(gray_image, 11, 17, 17) 
 cv2.imshow("smoothened image", gray_image)
 cv2.waitKey(0)
 
+#Detecting the edges of the smoothened image
 edged = cv2.Canny(gray_image, 30, 200) 
 cv2.imshow("edged image", edged)
 cv2.waitKey(0)
 
+#Finding the contours from the edged image
 cnts,new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 image1=image.copy()
 cv2.drawContours(image1,cnts,-1,(0,255,0),3)
 cv2.imshow("contours",image1)
 cv2.waitKey(0)
 
+#Sorting the identified contours
 cnts = sorted(cnts, key = cv2.contourArea, reverse = True) [:30]
 #screenCnt = None
 screenCnt = 0
@@ -57,24 +61,26 @@ cv2.drawContours(image2,cnts,-1,(0,255,0),3)
 cv2.imshow("Top 30 contours",image2)
 cv2.waitKey(0)
 
+#Finding the contour with four sides
 i=7
 for c in cnts:
         perimeter = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.018 * perimeter, True)
         if len(approx) == 4: 
                 screenCnt = approx
+        #Cropping the rectangular part identified as license plate
         x,y,w,h = cv2.boundingRect(c) 
         new_img=image[y:y+h,x:x+w]
         cv2.imwrite('./'+str(i)+'.png',new_img)
         i+=1
         break
 
-
-#cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
+#Drawing the selected contour on the original image
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
 cv2.imshow("image with detected license plate", image)
 cv2.waitKey(0)
 
-
+#Extracting text from the image of the cropped license plate
 Cropped_loc = './7.png'
 cv2.imshow("cropped", cv2.imread(Cropped_loc))
 plate = pytesseract.image_to_string(Cropped_loc, lang='eng')
@@ -84,15 +90,9 @@ num = int(plate[-2])
 #print(len(plate))
 print(num)
 
-'''
-for element in plate:
-    print(element, end='*')
-print("\n")
-'''
-
 index = open("templates/index.html").read().format(p1=plate)
 
-#Date
+#Date comparison with number plate last digit
 x = datetime.datetime.now()
 date = x.strftime("%d")
 date = int(date)
@@ -110,7 +110,3 @@ elif(date%2!=0 and num%2!=0):
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
